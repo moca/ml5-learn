3351593fe75095c7412983533cd70be1fc906bfc
Arturo Mondragón <moca.art@gmail.com>
2021-03-04 18:42:44 +0100

    rear camera ?

--- sketch.js
+++ sketch.js
@@ -1,42 +1,46 @@
+let mobilenet;
+let classifier;
 let video;
-let detector;
-let detections = [];
+let label;
+let button;
 
-function preload(){
-  let modelName = 'cocossd'
-  console.log(`Loading model: ${modelName}`)
-  detector = ml5.objectDetector(modelName);
-  console.log("Model loaded !!")
+function modelReady(){
+  console.log("Classifier is loaded !");
+  // mobilenet.classify(gotResults);
+}
+
+function videoReady(){
+  console.log("video is ready!");
 }
 
 function gotResults(error, results){
-  if (error) {
+  if (error){
     console.log(error);
   } else {
-    detections = results;
+    // console.log(results);
+    label = results[0].label
+    mobilenet.classify(gotResults);
   }
-  detector.detect(video, gotResults);
 }
 
-function setup(){
-  createCanvas(640, 480);
+function setup() {
+  createCanvas(640, 550);
   video = createCapture(VIDEO);
-  video.size(640, 480);
-  video.hide()
-
-  detector.detect(video, gotResults);
+  video.hide();
+  background(0);
+  mobilenet = ml5.featureExtractor('mobilenet', modelReady);
+  classifier = mobilenet.classification(video, videoReady)
+  button = createButton('ukulele');
+  button.mousePressed( function() {
+    // classifier.addImage()
+  });
 }
 
-function draw(){
+function draw() {
+  background(0);
   image(video, 0, 0);
-  for(const obj of detections){
-    stroke(255, 0,0);
-    strokeWeight(4);
-    noFill();
-    rect(obj.x, obj.y, obj.width, obj.height);
-    noStroke();
-    fill(0);
-    textSize(24)
-    text(obj.label, obj.x + 10, obj.y + 34 )
-  }
+
+  fill(255);
+  textSize(32);
+  text(label, 10, height - 40);
 }
