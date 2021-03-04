fc965c4883db2bb92b7a7520b9fe319ff0d01fe5
Arturo Mondragón <moca.art@gmail.com>
2021-03-04 14:54:50 +0100

    mobilenet done

--- sketch.js
+++ sketch.js
@@ -1,39 +1,5 @@
-let mobilenet;
-let puffin;
+let img;
 
-function clsReady(){
-  console.log("Classifier is loaded !");
-  mobilenet.classify(puffin, gotResults);
-}
-
-function gotResults(error, results){
-  if (error){
-    console.log(error);
-  } else {
-    console.log(results);
-    let label = results[0].label
-    fill(0);
-    textSize(64);
-    text(label, 10, height - 100);
-    createP(label);
-    
-  }
-}
-
-function imgReady(){
-  image(puffin, 0, 0, width, height)
-}
-
-function setup() {
-  createCanvas(640, 480);
-  puffin = createImg('./img/puffin.jpg', imgReady);
-  puffin.hide()
-
-  background(0);
-
-
-  mobilenet = ml5.imageClassifier('MobileNet', clsReady);
-}
-
-function draw() {
+function preload(){
+  img = loadImage('')
 }
