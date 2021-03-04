6cb81564b7fc19e92d03b6b98b88c0d8d4f940e1
Arturo Mondragón <moca.art@gmail.com>
2021-03-04 15:36:39 +0100

    coco from image

--- sketch.js
+++ sketch.js
@@ -1,5 +1,32 @@
 let img;
+let detector;
 
 function preload(){
-  img = loadImage('')
+  img = loadImage('img/dogncat_small.jpg');
+  detector = ml5.objectDetector('cocossd');
+}
+
+function gotResults(error, results){
+  if (error) {
+    console.log(error);
+  } else {
+    for(const obj of results){
+      console.log(obj);
+      stroke(0, 255,0);
+      strokeWeight(4);
+      noFill();
+      rect(obj.x, obj.y, obj.width, obj.height);
+      console.log(obj.x, obj.y, obj.width, obj.height);
+      noStroke();
+      fill(0);
+      textSize(24)
+      text(obj.label, obj.x + 10, obj.y + 34 )
+    }
+  }
+}
+
+function setup(){
+  createCanvas(640, 480);
+  image(img, 0,0, width, height);
+  detector.detect(img, gotResults);
 }
